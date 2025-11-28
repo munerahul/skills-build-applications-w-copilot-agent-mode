67abd21bd5dd510c2c6a94ae4c5fcac98b6fa91c
munerahul <rahul.raju.mune@accenture.com>
2025-11-28 10:13:19 +0000

    Update settings and urls for codespace and localhost API endpoints

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-h@y$yg3x)*r@j7@mnxk_w@0qrak86+i#*76-#x_ec0-_ar*nb_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME', '')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
