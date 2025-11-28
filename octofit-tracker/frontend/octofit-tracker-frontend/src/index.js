7136e8c98c96ab9db98b1ea9f3a0204ed4dc23c4
munerahul <rahul.raju.mune@accenture.com>
2025-11-28 10:19:42 +0000

    Add React components and navigation, connect to backend REST API

--- octofit-tracker/frontend/octofit-tracker-frontend/src/index.js
+++ octofit-tracker/frontend/octofit-tracker-frontend/src/index.js
@@ -5,6 +5,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Set REACT_APP_CODESPACE_NAME from environment if available
+if (!process.env.REACT_APP_CODESPACE_NAME && window.location.hostname.includes('github.dev')) {
+  const codespace = window.location.hostname.split('-')[0];
+  process.env.REACT_APP_CODESPACE_NAME = codespace;
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -12,7 +18,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
